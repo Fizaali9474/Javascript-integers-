c01892e93a1d02db45b6ec6858e6bf2f6a8a24bb
Fiza Ali <140485687+Fizaali9474@users.noreply.github.com>
2024-03-28 06:30:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+/*  var num1=+prompt("Enter value 1")
+var num2=+prompt("Enter value 2")
+add=num1 + num2
+ alert('Sum Of 13 and 9 is'+ add )
+
+ var num1=+prompt("Enter value 1")
+ var num2=+prompt("Enter value 2")
+ subtract=num1 - num2
+  alert('Result Of Subtracting 9 from 13 is'+ subtract)
+
+
+  var num1=+prompt("Enter value 1")
+  var num2=+prompt("Enter value 2")
+  Multiply=num1 * num2
+   alert('Result Of Multiplying 13 and 9 is'+ Multiply )
+
+
+   var num1=+prompt("Enter value 1")
+   var num2=+prompt("Enter value 2")
+   division=num1 / num2
+    alert('Result of dividing 13 by 9 is'+ division )
+
+    var num1=+prompt("Enter value 1")
+    var num2=+prompt("Enter value 2")
+    modulus=num1 % num2
+     alert('Reminder Of dividing 13 by 9 is'+ modulus ) 
+
+var ticketprice=600
+var numoftickets=5
+Multiply=600*5
+document.write('Total Cost to buy 5 tickets to a movie is'+ Multiply + 'PKR')
+
+
+Document.write('4 x 1' + '=' + '4') 
+Document.write('4 x 2' + '=' + '8') 
+Document.write('4 x 3' + '=' + '12') 
+Document.write('4 x 4' + '=' + '16') 
+Document.write('4 x 5' + '=' + '20') 
+Document.write('4 x 6' + '=' + '24') 
+Document.write('4 x 7' + '=' + '28') 
+Document.write('4 x 8' + '=' + '32') 
+Document.write('4 x 9' + '=' + '36') 
+Document.write('4 x 10' + '='+ '40') 
+
+
+
+
+
+
+
+var celsius=10
+var fahrenheit=celsius
+
+document.write('12C is 53.6F')
+
+var fahrenheit=10
+var celsius=fahrenheit
+
+document.write('68F is 20C' ) */
+
+
